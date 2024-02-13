4dcb2bcb55924c01f239f2c145321a931b358210
Martijn Weverling <mweverli@student.codam.nl>
2024-02-13 02:02:04 +0100

    added get_map

--- src/parser/get_map.c
+++ src/parser/get_map.c
@@ -8,8 +8,22 @@
 
 bool get_map(t_cub3d *info, char **line, ssize_t *idx)
 {
-	(void)(info);
-	(void)(line);
-	(void)(idx);
+	ssize_t i;
+
+	i = *idx;
+	while (line[i])
+		i++;
+	info->map = ft_calloc((i - *idx) + 1, sizeof(char *));
+	if (!info->map)
+		return (cbd_error(ERR_MEMORY), FAILURE);
+	i = 0;
+	while (line[*idx])
+	{
+		info->map[i] = ft_strdup(line[*idx]);
+		if (info->map[i] == NULL)
+			return (cbd_error(ERR_MEMORY), FAILURE);
+		i++;
+		(*idx)++;
+	}
 	return (SUCCESS);
 }
