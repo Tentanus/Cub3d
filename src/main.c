2df18d02b0ccca0c7e717420b8d3948845d4eb15
Acco Bartelds <abarteld@f0r1s2.codam.nl>
2024-03-19 17:01:02 +0100

    added reminder to reformat error messages, added raycaster function

--- src/main.c
+++ src/main.c
@@ -33,11 +33,12 @@ static int	validateinput(int argc, char **argv)
 
 int	main(int argc, char *argv[])
 {
+// All errors must be formatted as Error\n + explicit error message
 	t_cub3d	info;
 
 	if (parser(validateinput(argc, argv), &info) == FAILURE)
 		return (FAILURE);
-
+	raycaster(&info);
 	mlx_terminate(info.mlx);
 	return (SUCCESS);
 }
