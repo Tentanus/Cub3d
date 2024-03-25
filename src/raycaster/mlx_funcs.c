7d708c268ce8af35440c2a69f74b647df6278134
Acco Bartelds <abarteld@f0r1s2.codam.nl>
2024-03-25 15:02:13 +0100

    added various small mlx functionality and raycaster preparations

--- src/raycaster/mlx_funcs.c
+++ src/raycaster/mlx_funcs.c
@@ -10,18 +10,43 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "CBDerror.h"
+// #include "CBDerror.h"
 #include "Cub3d.h"
 #include "MLX42.h"
 
-bool	get_mlx(t_cub3d *info)
+bool	load_textures(t_raycaster *raycaster, const t_param param)
+{
+	mlx_texture_t	*texture;
+	int				i;
+
+	raycaster->textures[NORTH] = mlx_load_png(param.text_no);
+	raycaster->textures[EAST] = mlx_load_png(param.text_ea);
+	raycaster->textures[SOUTH] = mlx_load_png(param.text_so);
+	raycaster->textures[WEST] = mlx_load_png(param.text_we);
+	i = 0;
+	while (i < 4)
+	{
+		if (!raycaster->textures[i])
+			return (cbd_error(ERR_MEMORY), FAILURE);
+		i++;
+	}
+// mlx_delete_texture(texture);
+	return (SUCCESS);
+}
+
+bool	get_image(t_raycaster *raycaster)
+{
+	raycaster->image = mlx_new_image(raycaster->mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
+	if (!raycaster->image)
+		return (cbd_mlx_error(), mlx_terminate(raycaster->mlx), FAILURE);
+	return (SUCCESS);
+}
+
+bool	get_mlx(t_raycaster *raycaster)
 {
 	mlx_set_setting(MLX_STRETCH_IMAGE, true);
-	info->ray->mlx = mlx_init(WINDOW_WIDTH, WINDOW_HEIGHT, "Cub3d", false);
-	if (!info->ray->mlx)
+	raycaster->mlx = mlx_init(WINDOW_WIDTH, WINDOW_HEIGHT, "Cub3d", false);
+	if (!raycaster->mlx)
 		return (cbd_mlx_error(), FAILURE);
-	info->ray->image = mlx_new_image(info->ray->mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
-	if (!info->ray->image)
-		return (cbd_mlx_error(), mlx_terminate(info->ray->mlx), FAILURE);
 	return (SUCCESS);
 }
